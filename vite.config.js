503c64177c547e8a78010ee81a15e233a4042924
Emmanuel Canizzaro <emmacanizzaro@gmail.com>
2026-05-31 11:32:21 -0300

    Modularización: header, footer y logo como componentes HTML + integración con vite-plugin-html-include. Corrección de configuración y dependencias.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from "vite";
 
-export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/DevIAR-Web/" : "/",
-  server: {
-    port: 5173,
-    open: true,
-  },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
-}));
+import { defineConfig } from "vite";
+import htmlInclude from "vite-plugin-html-include";
+
+export default defineConfig(({ command }) => {
+  return {
+    base: command === "build" ? "/DevIAR-Web/" : "/",
+    plugins: [htmlInclude()],
+    server: {
+      port: 5173,
+      open: true,
+    },
+    build: {
+      outDir: "dist",
+      emptyOutDir: true,
+    },
+  };
+});
